fd08ebe7f5df8a56f28066c702a419ca85087688
seohyon-cho <flanique@naver.com>
2024-01-29 10:01:02 +0900

    header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
+import Image from 'next/image';
+import logo from '@/public/logo.svg';
 const Header = () => {
-  return (
-    <header className="w-[100%]">
-      <div className="w-[100%] flex justify-between items-center space-x-2 p-5">
-        <h1>NETFLIX</h1>
+	return (
+		<header className='w-full'>
+			<div className=' flex justify-between items-center space-x-2'>
+				<h1>
+					<Image src={logo} alt={'netflix'} width={100} height={100} className='cursor-pointer' />
+				</h1>
 
-        <ul className="flex gap-8">
-          <li className="headerLink">menu</li>
-          <li className="headerLink">menu</li>
-          <li className="headerLink">menu</li>
-        </ul>
-      </div>
-    </header>
-  );
+				<ul className='flex gap-8'>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+				</ul>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
