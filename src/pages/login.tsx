edb06e08f322a76329c80de4615012d6f4c6ad2e
seohyon-cho <flanique@naver.com>
2024-01-30 10:57:13 +0900

    로그인배경이미지에 로딩바연결

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
+import { BounceLoader } from 'react-spinners';
+import { useState } from 'react';
 
 const Login: FunctionComponent = () => {
+	const [IsLoading, setIsLoading] = useState(true);
 	return (
 		<main>
 			<Head>
@@ -10,9 +13,21 @@ const Login: FunctionComponent = () => {
 				<link rel='icon' href='/public/favicon.ico' />
 			</Head>
 
-			<Image src='https://rb.gy/p2hphi' alt='login Page' fill priority className='w-full h-screen z-[10] opacity-50 object-cover' />
+			<div className='flex items-center justify-center w-full h-screen '>
+				<Image
+					onLoadingComplete={() => setIsLoading(false)}
+					src='https://rb.gy/p2hphi'
+					alt='login Page'
+					fill
+					priority
+					className='object-cover w-full h-screen opacity-50'
+				/>
+
+				<BounceLoader size={100} color='orange' loading={IsLoading} />
+			</div>
 		</main>
 	);
 };
 
 export default Login;
+// npm install react-spinners --save
